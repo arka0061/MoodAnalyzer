9ded2060e0aa172ad0cb0bd0010f203216af8d2c
Arka <arka.spectacular@gmail.com>
2021-08-14 00:41:57 +0530

    [Arka]Add:Added mood analyze function

---
+++ MoodAnalyzer/MoodAnalyzerMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyzerMain
+    {
+        public MoodAnalyzerMain()
+        {
+
+        }
+        public string analyzeMood(string message)
+        {
+            message = message.ToLower();
+            if (message.Contains("happy"))
+                return "Happy";
+            else
+                return "Sad";
+        }
+        
+    }
+}
