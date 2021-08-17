bbaf61c1f95d5c4f5e78698c7ab042848358959f
Arka <arka.spectacular@gmail.com>
2021-08-17 15:23:01 +0530

    [Arka]Add:Mood Analyzer Parameterized Constructor Check

--- MoodAnalyzer/MoodAnalyzerFactory.cs
+++ MoodAnalyzer/MoodAnalyzerFactory.cs
@@ -31,5 +31,33 @@ namespace MoodAnalyzer
                 throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.CONSTRUCTOR_NOT_FOUND, "Constructor not found");
             }
         }
+        public static object CreateMoodAnalyserParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = Type.GetType(className);
+            try
+            {
+                if (type.FullName.Equals(className) || type.Name.Equals(className))
+                {
+                    if (type.Name.Equals(constructorName))
+                    {
+                        ConstructorInfo info = type.GetConstructor(new[] { typeof(string) });
+                        object instance = info.Invoke(new object[] { message });
+                        return instance;
+                    }
+                    else
+                    {
+                        throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.CONSTRUCTOR_NOT_FOUND, "Constructor not found");
+                    }
+                }
+                else
+                {
+                    throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.CLASS_NOT_FOUND, "Class not found");
+                }
+            }
+            catch (Exception e)
+            {
+                return e;
+            }
+        }
     }
 }
